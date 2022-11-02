c240a932b6de0195c66221b1390adb767dc33890
Peter Rifkind <prifkind@gmail.com>
2022-11-02 15:05:00 -0400

    Added start script to package.json

--- deploy.js
+++ deploy.js
@@ -3,9 +3,9 @@ const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
-  'REPLACE_WITH_YOUR_MNEMONIC',
+  'you key here',
   // remember to change this to your own phrase!
-  'https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c'
+  'your link here'
   // remember to change this to your own endpoint!
 );
 const web3 = new Web3(provider);
@@ -20,6 +20,8 @@ const deploy = async () => {
     .send({ gas: '1000000', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
+  console.log('Interface: ', interface);
+
   provider.engine.stop();
 };
 deploy();
